1c6a1317869e67bd14240672e353066e45f4dc5f
Khuong Duy <140906531+duyk04@users.noreply.github.com>
2025-09-15 13:48:33 +0700

    Add Tailwind CSS setup to project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,59 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import { list } from 'postcss';
+
+  export default {
+   data() {
+     return {
+       msg: 'Hello Vue 3 + Vite + Tailwind CSS',
+       status: true,
+       count: 0,
+       list: ['a', 'b', 'c']
+     }
+   },
+  };
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+    <h1 class="text-4xl font-bold mb-4">{{ msg }}</h1>
+    <button 
+      @click="status = !status" 
+      class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition"
+    >
+      Toggle Status
+    </button>
+    <p class="mt-4 text-xl" :class="status ? 'text-green-500' : 'text-red-500'">
+      Status is: {{ status ? 'Active' : 'Inactive' }}
+    </p>
+    <div>
+      <button 
+        @click="count++" 
+        class="mt-6 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 transition"
+      >
+        Increment Count
+      </button>
+      <button 
+        @click="count--" 
+        class="mt-6 ml-4 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
+      >
+        Decrement Count
+      </button>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <button 
+        @click="count = 0" 
+        class="mt-6 ml-4 px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition"
+      >
+        Reset Count
+      </button>
+
+      <p class="text-center mt-4" >{{ count }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
+  <!-- <div>
+    <ul class="mt-6 list-disc list-inside">
+      <li v-for="(item, index) in list" :key="index" class="text-lg">
+        {{ item }}
+      </li>
+    </ul>
+  </div> -->
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
